df0f71725729e0a9cd0fd87f67a35652f88fc363
Amit Kumar <amity1401@gmail.com>
2025-03-10 17:35:00 +0530

    updated display banner

--- arch.sh
+++ arch.sh
@@ -2,8 +2,20 @@
 
 set -e
 
-echo "Welcomoe to the Arch Linux Installer!"
-echo -e "                created by Amit Kumar\n\n"
+# Define color variables
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BOLD_WHITE='\033[1;37m'
+NC='\033[0m'  # No Color
+
+# Display banner
+echo -e "${RED}**************************************************${NC}"
+echo -e "${RED}*                                                *${NC}"
+echo -e "${RED}*       ${BOLD_WHITE}WELCOME TO THE ARCH LINUX INSTALLER!${RED}       *${NC}"
+echo -e "${RED}*                                                *${NC}"
+echo -e "${RED}**************************************************${NC}"
+echo -e "${RED}*              ${GREEN}created by Amit Kumar${RED}              *${NC}"
+echo -e "${RED}**************************************************${NC}"
 
 # Sync and install necessary packages
 pacman -Sy archlinux-keyring --noconfirm
